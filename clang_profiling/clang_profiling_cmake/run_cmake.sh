cd24b1e9a1bf0bfa58e82ad9c64ecf90735e1476
atifkarim <atif0902082@gmail.com>
2022-10-05 15:14:23 +0200

    added steps of doing gprof profiling

--- clang_profiling/clang_profiling_cmake/run_cmake.sh
+++ clang_profiling/clang_profiling_cmake/run_cmake.sh
@@ -13,5 +13,12 @@ LLVM_PROFILE_FILE="clang_prof_exec.profraw" ./clang_prof_exec
 llvm-profdata merge -sparse clang_prof_exec.profraw -o clang_prof_exec.profdata
 
 # following commands are needed to investigate profiling output. Use any of these
-llvm-cov show ./clang_prof_exec -instr-profile=clang_prof_exec.profdata
-# llvm-cov report ./example_chain_prof -instr-profile=example_chain_prof.profdata
+# llvm-cov show ./clang_prof_exec -instr-profile=clang_prof_exec.profdata
+llvm-cov report ./clang_prof_exec -instr-profile=clang_prof_exec.profdata
+
+# gprof profiling tool for making analysis file
+# This portion should be executed if one time the executable is run which has already done
+# in "LLVM_PROFILE_FILE="clang_prof_exec.profraw" ./clang_prof_exec"
+# Actually, it requires only perform of : ./clang_prof_exec
+
+gprof clang_prof_exec gmon.out > analysis.txt
