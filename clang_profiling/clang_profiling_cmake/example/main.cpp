41d9d44b635d7be491e4604db36856297addc066
atifkarim <atif0902082@gmail.com>
2022-10-05 14:42:36 +0200

    reduced long calc

--- clang_profiling/clang_profiling_cmake/example/main.cpp
+++ clang_profiling/clang_profiling_cmake/example/main.cpp
@@ -7,15 +7,15 @@ int main()
     std::cout << "Inside main()" << std::endl;
     uint32_t d = 0;
 
-    for(uint32_t i = 0; i < static_cast<uint32_t>(((static_cast<uint64_t>(1) << 32) - static_cast<uint64_t>(1)));i++)
+    for(uint32_t i = 0; i < static_cast<uint32_t>(((static_cast<uint64_t>(1) << 10) - static_cast<uint64_t>(1)));i++)
     {
         d++;
     }
 
     std::cout << "d: " << d << std::endl;
-    func1();
-    func2();
-    func2();
+    // func1();
+    // func2();
+    // func2();
 
     return 0;
 }
